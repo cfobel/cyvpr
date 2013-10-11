99ba70583fc6d78ac3faab94fe76e5f61b187137
Christian Fobel <christian@fobel.net>
2013-10-11 17:59:52 -0400

    Add `evaluate` kwarg to BoundFinderBase init,...
    Add example _(in `Bisect` IPython notebook)_ showing step-finder vs.
    bisect-finder in the case where the step-finder takes fewer steps to
    find the boundary-of-interest.

--- cyvpr/bisect.py
+++ cyvpr/bisect.py
@@ -1,29 +1,82 @@
 import sys
 
 
-class BisectBase(object):
-    def _starting_bad(self):
-        raise NotImplementedError
+class BoundFinderBase(object):
+    '''
+    This class provides an API for finding a boundary-value for a monotonic
+    function, _i.e., an upper-bound or lower-bound on a parameter that results
+    in a successful evaluation of the user-defined `evaluate` method.
 
-    def _starting_good(self):
-        raise NotImplementedError
+    By calling the `start` method with some initial value, several iterations
+    of evaluation are performed to find the boundary value of interest.
 
+    __NB__ This class is an abstract-base-class, since it does not provide a
+    default implementation for the following methods:
+
+      * `evaluate`
+      * `_next_after_bad`
+      * `_next_after_good`
+    '''
     def evaluate(self, value):
-        raise NotImplementedError
-
-    def _next_after_good(self, value):
+        '''
+        Return `True` to indicate success for the provided value, or `False` to
+        indicate failure.  This result is used to guide the search to the
+        boundary of interest.
+        '''
         raise NotImplementedError
 
     def _next_after_bad(self, value):
+        '''
+        Given a value that failed evaluation, propose a new value to evaluate.
+        '''
         raise NotImplementedError
 
+    def _next_after_good(self, value):
+        '''
+        Given a value that resulted in a successful evaluation, propose a new
+        value to evaluate to try to tighten the bound.
+        '''
+        raise NotImplementedError
+
+
+    def __init__(self, known_bad, known_good, evaluate=None):
+        if evaluate is not None:
+            self.evaluate = evaluate
+        self.known_bad = known_bad
+        self.known_good = known_good
+
+    def _starting_bad(self):
+        return self.known_bad
+
+    def _starting_good(self):
+        return self.known_good
+
     def _reset(self, max_iterations):
+        '''
+        Reset the state of the finder to prepare for a new search.
+        '''
         self.bad = self._starting_bad()
         self.good = self._starting_good()
         self.max_iterations = max_iterations
         self._iteration = 0
+        self._good_found = False
 
     def _run(self, start_value):
+        '''
+        Perform a recursive search to find the boundary of interest, by
+        successively evaluating potential values.  After each evaluation, the
+        `_next_after_bad` and `_next_after_good` methods are used to determine
+        the next value to try.
+
+        __NB__ TODO: Look up terminology for recursive strategies in
+        "Structured Parallel Programming".  They provide a term for what I call
+        `exit` here.
+
+        The recursive calls exit when the proposed test value is the same as
+        the most-recently tested value that failed the evaluation.  In this
+        case, we know that we have encountered the transition from `good` to
+        `bad`, _i.e., we have found the boundary_.
+        '''
         self._iteration += 1
         if self._iteration >= self.max_iterations:
             raise IndexError, ('Maximum number of iterations (%d) reached.' %
@@ -38,35 +91,62 @@ class BisectBase(object):
             self.bad = start_value
             return self._run(self._next_after_bad(start_value))
         else:
+            self._good_found = True
             self.good = start_value
             return self._run(self._next_after_good(start_value))
 
-    def start(self, start_value, max_iterations=10):
+    def start(self, start_value, max_iterations=50):
+        '''
+        Reset the state of the current instance to prepare for a new search and
+        start the recursive search by calling the `_run` method.
+
+        __NB__ An optional number of maximum iterations may be specified.  If
+        the search takes more than the specified number of iterations before
+        finding the boundary-of-interest, an `IndexError` is raised.
+        '''
         self._iteration = 0
         self._reset(max_iterations)
         return self._run(start_value)
 
 
-class BinaryLeftBisect(BisectBase):
-    def __init__(self, lower_bound=0):
-        self.lower_bound = lower_bound
+class LowerBoundFinderBase(BoundFinderBase):
+    '''
+    Find the lower-bound of values for which the method `evaluate` returns
+    `True`.
+    '''
+    def __init__(self, **kwargs):
+        known_bad = kwargs.pop('known_bad', 0)
+        known_good = kwargs.pop('known_good', sys.maxint)
+        super(LowerBoundFinderBase, self).__init__(known_bad, known_good,
+                                                   **kwargs)
 
-    def evaluate(self, value):
-        raise NotImplementedError
 
-    def _starting_bad(self):
-        return self.lower_bound
+class UpperBoundFinderBase(BoundFinderBase):
+    '''
+    Find the upper-bound of values for which the method `evaluate` returns
+    `True`.
+    '''
+    def __init__(self, **kwargs):
+        known_bad = kwargs.pop('known_bad', sys.maxint)
+        known_good = kwargs.pop('known_good', 0)
+        super(UpperBoundFinderBase, self).__init__(known_bad, known_good,
+                                                   **kwargs)
 
-    def _starting_good(self):
-        return sys.maxint
 
+class LowerBoundBisectFinder(LowerBoundFinderBase):
     def _next_after_good(self, value):
-        # Since the current run was successful, try to find a lower successful
-        # value between the current value and the most-recently-failed value.
+        '''
+        Since the current run was successful, try to find a lower successful
+        value between the current value and the most-recently-failed value.
+        '''
         return (self.good - self.bad) / 2 + self.bad
 
     def _next_after_bad(self, value):
-        if self.good == sys.maxint:
+        '''
+        Keep doubling the last attempted value until we find a successful
+        evaluation.
+        '''
+        if not self._good_found:
             # We haven't processed a successful evaluation yet, so double
             # the start-value and try again.
             return value * 2
@@ -78,27 +158,17 @@ class BinaryLeftBisect(BisectBase):
             return self._next_after_good(value)
 
 
-class BinaryRightBisect(BisectBase):
-    def __init__(self, upper_bound=sys.maxint):
-        self.upper_bound = upper_bound
-
-    def evaluate(self, value):
-        raise NotImplementedError
-
-    def _starting_bad(self):
-        return self.upper_bound
-
-    def _starting_good(self):
-        return 0
-
+class UpperBoundBisectFinder(UpperBoundFinderBase):
     def _next_after_good(self, value):
-        # Since the current run was successful, try to find a higher successful
-        # value between the current value and the most-recently-failed value.
+        '''
+        Since the current run was successful, try to find a higher successful
+        value between the current value and the most-recently-failed value.
+        '''
         next_value = max((self.bad - self.good) / 2, 1) + self.good
         return next_value
 
     def _next_after_bad(self, value):
-        if self.good == 0:
+        if not self._good_found:
             # We haven't processed a successful evaluation yet, so double
             # the start-value and try again.
             next_value = value / 2
@@ -111,21 +181,87 @@ class BinaryRightBisect(BisectBase):
         return next_value
 
 
-class BisectLowerBound(BinaryLeftBisect):
-    def __init__(self, target_lower_bound, lower_bound=0):
-        self.target_lower_bound = target_lower_bound
-        super(BisectLowerBound, self).__init__(lower_bound)
+class LowerBoundStepFinder(LowerBoundFinderBase):
+    def __init__(self, *args, **kwargs):
+        self.step = kwargs.pop('step', 1)
+        super(LowerBoundStepFinder, self).__init__(*args, **kwargs)
 
+    def _next_after_good(self, value):
+        '''
+        Since the current run was successful, try to find a lower successful
+        value between the current value and the most-recently-failed value.
+        '''
+        return (self.good - self.bad) / 2 + self.bad
+
+    def _next_after_bad(self, value):
+        '''
+        Keep incrementing the last attempted value by `self.step` until we find
+        a successful evaluation.
+        '''
+        if not self._good_found:
+            # We haven't processed a successful evaluation yet, so increment
+            # the start-value by `self.step` and try again.
+            next_value = value + self.step
+        else:
+            next_value = self._next_after_good(value)
+        return next_value
+
+
+class UpperBoundStepFinder(UpperBoundFinderBase):
+    def __init__(self, *args, **kwargs):
+        self.step = kwargs.pop('step', 1)
+        super(UpperBoundStepFinder, self).__init__(*args, **kwargs)
+
+    def _next_after_good(self, value):
+        '''
+        Since the current run was successful, try to find a higher successful
+        value between the current value and the most-recently-failed value.
+        '''
+        next_value = max((self.bad - self.good) / 2, 1) + self.good
+        return next_value
+
+
+    def _next_after_bad(self, value):
+        '''
+        Keep decrementing the last attempted value by `self.step` until we find
+        a successful evaluation.
+        '''
+        if not self._good_found:
+            # We haven't processed a successful evaluation yet, so decrement
+            # the start-value by `self.step` and try again.
+            next_value = value - self.step
+        else:
+            # There has been at least one value that has resulted in a
+            # successful evaluation.  Since the current value failed
+            # evaluation, try a value half-way between the current value
+            # and the lowest successful value encountered so far.
+            next_value = self._next_after_good(value)
+        return next_value
+
+
+class BisectLowerBound(LowerBoundBisectFinder):
     def evaluate(self, value):
-        result = (value >= self.target_lower_bound)
+        result = (value >= self.known_good)
+        print '[evaluate] value=%s (%s)' % (value, result)
         return result
 
 
-class BisectUpperBound(BinaryRightBisect):
-    def __init__(self, target_upper_bound, upper_bound=sys.maxint):
-        self.target_upper_bound = target_upper_bound
-        super(BisectUpperBound, self).__init__(upper_bound)
-
+class BisectUpperBound(UpperBoundBisectFinder):
     def evaluate(self, value):
-        result = (value <= self.target_upper_bound)
+        result = (value <= self.known_good)
+        print '[evaluate] value=%s (%s)' % (value, result)
+        return result
+
+
+class StepLowerBound(LowerBoundStepFinder):
+    def evaluate(self, value):
+        result = (value > self.known_bad and value >= self.known_good)
+        print '[evaluate] value=%s (%s)' % (value, result)
+        return result
+
+
+class StepUpperBound(UpperBoundStepFinder):
+    def evaluate(self, value):
+        result = (value >= self.known_good and value < self.known_bad)
+        print '[evaluate] value=%s (%s)' % (value, result)
         return result
