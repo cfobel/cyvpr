81e5fa58867a8f3443fbe20559a867990547a75d
Christian Fobel <christian@fobel.net>
2013-10-21 17:39:31 -0400

    Add dump_round_robin_placement_sha1s program
    Usage:
    
        dump_round_robin_placement_sha1s.py [-h] placement_tables_hdf_file
        dump_round_robin_placement_sha1s.py: error: too few arguments

---
+++ cyvpr/bin/dump_round_robin_placement_sha1s.py
@@ -0,0 +1,32 @@
+from pprint import pprint
+
+from path import path
+import tables as ts
+
+
+def round_robin_placement_rows(h5f):
+    placement_rows = sorted([(row.fetch_all_fields(), i)
+                              for t in h5f.walkNodes('/', 'Table')
+                              for i, row in enumerate(t)
+                              if t._v_name == 'placements'])
+    return placement_rows
+
+
+def parse_args():
+    """Parses arguments, returns (options, args)."""
+    from argparse import ArgumentParser
+    parser = ArgumentParser(description='Merge placement results HDF files.')
+    parser.add_argument(dest='placement_tables_hdf_file', type=path)
+    args = parser.parse_args()
+    return args
+
+
+
+if __name__ == '__main__':
+    args = parse_args()
+    h5f = ts.openFile(str(args.placement_tables_hdf_file), 'r')
+    try:
+        data = round_robin_placement_rows(h5f)
+        print '\n'.join([d[0]['block_positions_sha1'] for d in data])
+    finally:
+        h5f.close()
