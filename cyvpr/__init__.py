d79b0de70780279d24e002d4ef201f8a4108530d
Christian Fobel <christian@fobel.net>
2013-12-16 08:13:11 -0500

    Add helper functions for net-files by namebase...
    Add the following helper functions for dealing with net-files by namebase
    _(i.e., `ex5p`, `tseng`, `clma`, etc.)_:
    
      * `get_net_filepath_by_namebase(namebase)`
      * `get_parser_by_namebase(namebase)`
      * `get_netfile_info(namebase)`

--- cyvpr/__init__.py
+++ cyvpr/__init__.py
@@ -39,3 +39,35 @@ def get_data_root():
     import cyvpr
     return [os.path.abspath(os.path.join(os.path.dirname(cyvpr.__file__),
                                          'data'))]
+
+
+def get_net_filepath_by_namebase(namebase):
+    from path import path
+
+    filepath = path(get_data_root()[0]).joinpath('mcnc', '%s.net' %
+                                                 namebase).realpath()
+    assert(filepath.isfile())
+    return filepath
+
+
+def get_parser_by_namebase(namebase):
+    from vpr_netfile_parser.VprNetParser import cVprNetFileParser
+
+    return cVprNetFileParser(get_net_filepath_by_namebase(namebase))
+
+
+def get_netfile_info(namebase):
+    p = get_parser_by_namebase(namebase)
+    netfile_info = dict([('net_count', len(p.net_labels)),
+                         ('block_counts', dict([(k, len(v)) for k, v in
+                                                p.block_ids_by_type()
+                                                .items()]))])
+    netfile_info['io_count'] = sum([v for k, v in
+                                    netfile_info['block_counts'].items() if k
+                                    in ('.input', '.output')])
+    netfile_info['clb_count'] = sum([v for k, v in
+                                     netfile_info['block_counts'].items() if k
+                                     in ('.clb', )])
+    netfile_info['clb_to_io_ratio'] = (float(netfile_info['clb_count']) /
+                                       netfile_info['io_count'])
+    return netfile_info
